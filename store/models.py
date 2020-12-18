cdc9dd2e055e72636f729ee6ae29c5bec6ed829f
Anas Rabai <20316@ecam.be>
2020-12-18 15:53:27 +0100

    Render cart

--- store/models.py
+++ store/models.py
@@ -42,6 +42,21 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    # get_cart_total returns the total price of the entire cart
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    # get_cart_total returns the total quantity of the entire cart
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
+
 # OrderItem has a ManyToOne relationship with the Order
 # OrderItem is a child of Order
 class OrderItem(models.Model):
@@ -49,6 +64,12 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete= models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateField(auto_now_add=True)
+
+    # get_total gives the total price for one item (price * quantity)
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 
