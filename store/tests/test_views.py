da4e85539aeb186f2a9e6a34e839ed6c4f1d12d8
Anas Rabai <20316@ecam.be>
2020-12-23 18:46:59 +0100

    Test views

---
+++ store/tests/test_views.py
@@ -0,0 +1,33 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from store.models import Customer, Product, Order, OrderItem, ShippingAddress
+import json
+
+
+# TestViews tests that the response get from the server is 200
+# TestViews tests that views render the exact html
+class TestViews(TestCase):
+
+
+    def test_store_GET(self):
+        client = Client()
+        response = client.get(reverse('store'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'store/store.html')
+
+
+    def test_cart_GET(self):
+        client = Client()
+        response = client.get(reverse('cart'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'store/cart.html')
+
+
+    def test_checkout_GET(self):
+        client = Client()
+        response = client.get(reverse('checkout'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'store/checkout.html')
